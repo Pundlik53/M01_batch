e74f0a895dab7b116e124c10a3043da3b83f75d5
Pundlik53 <bansodpundlik96@gmail.com>
2025-04-19 11:50:38 +0530

    Create M1.java

---
+++ M1.java
@@ -0,0 +1,11 @@
+public class Xpath_by_attribritute_AND_Contains_method {
+
+	public static void main(String[] args) throws InterruptedException 
+	
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		}
+}
